5eab71ee47407a61661b17646c582a8cc22e5e80
Scrappydoo112 <scrappydoo112343@gmail.com>
2025-04-23 13:19:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function replaceAll(find,replace,str){return str.replace(new RegExp(escapeRegExp
 function cap(str){return str.charAt(0).toUpperCase()+str.slice(1);}
 
 // Define the interval for automatic clicks (in milliseconds)
-const clickInterval = 0.1; // 10ms means 100 clicks per second
+const clickInterval = 1; // 10ms means 100 clicks per second
 
 // Function to simulate a click on the big cookie
 function autoClick() {
